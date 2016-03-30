cdc803a1b8b21711ea121bad1c8ffef10f3ab1c4
Silviu Catarau <freemanfx@gmail.com>
2016-03-30 16:34:01 +0300

    first commit

---
+++ deviceInfo.js
@@ -0,0 +1,37 @@
+var exec = require('child_process').exec;
+var http = require('http');
+var adb = require('adbkit');
+var client = adb.createClient();
+
+const HTTP_PORT = '9000';
+var deviceListingCmd = 'adb devices -l';
+
+
+function handleRequest(req, res){
+  // exec(deviceListingCmd, function(error, stdout, stderr){
+  //   response.end(stdout);
+  // });
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+    'Expires': new Date().toUTCString()
+  });
+  res.write('List of devices:</br>')
+  client.listDevices()
+        .map(function(device){
+          return getDeviceInfo(device);
+        })
+        .then(function(devicesList){
+            devicesList.forEach(function(deviceInfo){
+              var model = deviceInfo['ro.product.model'];
+              res.write(model + '</br>');
+            })
+            res.end();
+        });
+}
+
+var server = http.createServer(handleRequest);
+server.listen(HTTP_PORT);
+
+function getDeviceInfo(device){
+  return client.getProperties(device.id);
+}
